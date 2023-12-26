30c14148e3b2e40059986b8a12f8ac8634fa6982
alonetoadvise <97840088+alonetoadvise@users.noreply.github.com>
2023-12-26 23:20:11 +0000

    Update EUsurvey20172022.py

--- EVS_HU_survey/EUsurvey20172022.py
+++ EVS_HU_survey/EUsurvey20172022.py
@@ -6,8 +6,3 @@ data = pd.read_csv(file)
 data_hu = data.groupby('cntry_an').get_group('HU')
 
 data_hu.to_csv("EVS_data_hu.csv")
-#print(data.isnull().sum())
-
-#print(data["cntry_an"].value_counts())
-
-#print(data_hu.dtypes)
